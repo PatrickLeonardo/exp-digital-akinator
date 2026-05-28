b8489b7dbf745c159d3b4d3638a8f184626aef4a
PatrickLeonardo <patricklf7@proton.me>
2026-05-28 00:05:40 -0300

    upload files

---
+++ atributos.js
@@ -0,0 +1,82 @@
+export default {
+  "humano": {
+    "atributo": "humano",
+    "pergunta": "O personagem é humano?"
+  },
+  "masculino": {
+    "atributo": "masculino",
+    "pergunta": "O personagem é do sexo masculino?"
+  },
+  "feminino": {
+    "atributo": "feminino",
+    "pergunta": "O personagem é do sexo feminino?"
+  },
+  "adulto": {
+    "atributo": "adulto",
+    "pergunta": "O personagem é adulto?"
+  },
+  "crianca": {
+    "atributo": "crianca",
+    "pergunta": "O personagem é uma criança?"
+  },
+  "usa_oculos": {
+    "atributo": "usa_oculos",
+    "pergunta": "O personagem usa óculos?"
+  },
+  "usa_chapeu": {
+    "atributo": "usa_chapeu",
+    "pergunta": "O personagem usa chapéu?"
+  },
+  "usa_vestido": {
+    "atributo": "usa_vestido",
+    "pergunta": "O personagem usa vestido?"
+  },
+  "cabelo_azul": {
+    "atributo": "cabelo_azul",
+    "pergunta": "O personagem possui cabelo azul?"
+  },
+  "cabelo_espetado": {
+    "atributo": "cabelo_espetado",
+    "pergunta": "O personagem possui cabelo espetado?"
+  },
+  "guloso": {
+    "atributo": "guloso",
+    "pergunta": "O personagem é guloso?"
+  },
+  "bagunceiro": {
+    "atributo": "bagunceiro",
+    "pergunta": "O personagem é bagunceiro?"
+  },
+  "inteligente_inventor": {
+    "atributo": "inteligente_inventor",
+    "pergunta": "O personagem é inteligente ou inventor?"
+  },
+  "medroso": {
+    "atributo": "medroso",
+    "pergunta": "O personagem é medroso?"
+  },
+  "bravo_temperamental": {
+    "atributo": "bravo_temperamental",
+    "pergunta": "O personagem é bravo ou temperamental?"
+  },
+  "mora_na_roca": {
+    "atributo": "mora_na_roca",
+    "pergunta": "O personagem mora na roça?"
+  },
+  "vive_no_espaco": {
+    "atributo": "vive_no_espaco",
+    "pergunta": "O personagem vive no espaço?"
+  },
+  "vive_na_pre_historia": {
+    "atributo": "vive_na_pre_historia",
+    "pergunta": "O personagem vive na pré-história?"
+  },
+  "sobrenatural": {
+    "atributo": "sobrenatural",
+    "pergunta": "O personagem possui características sobrenaturais?"
+  },
+  "animal": {
+    "atributo": "animal",
+    "pergunta": "O personagem é um animal?"
+  }
+}
---
+++ main.js
@@ -0,0 +1,100 @@
+import atributos from './atributos.js'
+import personagens from './personagens.js'
+
+const vetAtributos = [];
+
+for(let key in atributos) {
+    vetAtributos.push(atributos[key]);
+}
+
+let vetPersonagens = []
+
+for(let key in personagens) {
+    vetPersonagens.push(personagens[key]);
+}
+
+const choiceRandomQuestion = () => {
+    const random = parseInt(Math.random() * vetAtributos.length);
+    const choiced = vetAtributos[random];
+
+    return { "idx": random, "question": choiced };    
+}
+
+const answerOfQuestion = (answer, atributo) => {
+    
+    const buff = [];
+
+    vetPersonagens.map(personagem => {
+        
+        if(answer && personagem[atributo]) {
+            buff.push(personagem);
+        } else if(!personagem[atributo]) {
+            buff.push(personagem);
+        }
+
+    });
+
+    vetPersonagens = buff;
+
+}
+
+const removeAttributeByQuestionAndAnswer = (question, answer) => {
+    
+    const atributosDistintos = [
+        ["adulto", "crianca"],
+        ["masculino", "feminino"],
+        ["humano", "animal", "sobrenatural"],
+        ["vive_na_pre_historia", "vive_no_espaco", "mora_na_roca"]
+    ]
+    
+    let buffVetAtributos = false;
+
+    atributosDistintos.map(atributos => {
+        if(atributos.includes(question)) {
+            buffVetAtributos = atributos;
+        }
+    });
+     
+    if(buffVetAtributos) {
+        
+        buffVetAtributos.forEach(atributo => {
+            
+            if(question == atributo) {
+                if(!answer) {
+                    buffVetAtributos = [atributo];
+                }
+            }
+            
+        });
+        
+        buffVetAtributos.forEach(atributo => {
+            const idxToRemove = vetAtributos.findIndex(a => a["atributo"] == atributo);
+            vetAtributos.splice(idxToRemove, 1);
+        });
+        
+        return true;
+    }
+
+    return false;
+}
+
+const startLoop = () => {
+    
+    while(vetPersonagens.length != 1) {
+        
+        const choiced = choiceRandomQuestion();
+        const answer = confirm(choiced["question"]["pergunta"]);
+        
+        if(!removeAttributeByQuestionAndAnswer(choiced["question"]["atributo"], answer)) {
+            vetAtributos.splice(choiced["idx"], 1);
+        }
+
+        answerOfQuestion(answer, choiced["question"]["atributo"]);
+        
+    }
+
+    alert("Seu Personagem é " + vetPersonagens[0]["nome"])
+
+}
+
+startLoop();
